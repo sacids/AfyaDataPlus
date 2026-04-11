cc204c5089a84691eeddca80ee9d1d57469f85dd
mchizani <ericbeda@gmail.com>
2026-04-11 23:32:18 +0300

    major update

--- constants/config.js
+++ constants/config.js
@@ -1,12 +1,8 @@
 
 export const config = {
 
-    //BASE_URL: 'https://cromis.app.mkwawa.co.tz/api/v1/',
     BASE_URL: 'https://fao.afyadata.sacids.org',
     //BASE_URL: 'http://127.0.0.1:8000',
-    //BASE_URL: 'http://192.168.0.3:8000/api/v1/',
-    //BASE_URL: 'http://192.168.142.174:8000/api/v1/',
-    //BASE_URL: 'http://192.168.8.101:8000/api/v1',
     TOKEN_KEY: 'my_jwt',
 
 };
