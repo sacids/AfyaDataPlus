d8f3efae04f34829ef9918ba6a092bac8c02b721
mchizani <ericbeda@gmail.com>
2026-04-12 22:46:51 +0300

    update

--- constants/config.js
+++ constants/config.js
@@ -1,8 +1,8 @@
 
 export const config = {
 
-    //BASE_URL: 'https://fao.afyadata.sacids.org',
-    BASE_URL: 'http://10.157.145.174:8000',
+    BASE_URL: 'https://fao.afyadata.sacids.org',
+    //BASE_URL: 'http://10.157.145.174:8000',
     TOKEN_KEY: 'my_jwt',
 
 };
