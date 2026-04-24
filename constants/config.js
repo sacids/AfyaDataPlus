9e31a324799bd17a7688fd9de1cd54189c1b4d66
mchizani <ericbeda@gmail.com>
2026-04-24 07:56:26 +0000

    minor changes

--- constants/config.js
+++ constants/config.js
@@ -3,8 +3,8 @@ export const config = {
 
     //BASE_URL: 'https://hub.afyadata.sacids.org',
     BASE_URL: 'https://hub.afyadata.sacids.org',
-    //AFYADATA_HUB_URL: 'http://hub.afyadata.sacids.org',
-    AFYADATA_HUB_URL: 'http://10.216.186.174',
+    AFYADATA_HUB_URL: 'https://hub.afyadata.sacids.org',
+    //AFYADATA_HUB_URL: 'http://10.216.186.174',
     //BASE_URL: 'http://10.157.145.174:8000',
     TOKEN_KEY: 'my_jwt',
     AFYADATA_GLOBAL_KEY: "AFYADATA_GLOBAL_KEY"
